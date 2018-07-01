13fb4eda8cc843acc69886fb04401afba4535cc6
Edwin Watkeys <edw@poseur.com>
2018-07-01 11:47:59 -0400

    Initial Commit

---
+++ Parser/Parser.h
@@ -0,0 +1,19 @@
+//
+//  Parser.h
+//  Parser
+//
+//  Created by Edwin Watkeys on 7/1/18.
+//  Copyright © 2018 Thunk NYC Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Parser.
+FOUNDATION_EXPORT double ParserVersionNumber;
+
+//! Project version string for Parser.
+FOUNDATION_EXPORT const unsigned char ParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Parser/PublicHeader.h>
+
+
